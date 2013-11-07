66e74d10cbc81c6301858c416eaabc4de84c2eb9
Logan Hasson <logan.hasson@gmail.com>
2013-11-07 08:52:52 -0500

    Fixing local git repos

---
+++ simple.arrays.rb
@@ -0,0 +1,12 @@
+# Construct an array of your favorite foods
+foods = ["Dove Bars", "Pickles", "Hot Pastrami on Rye", "Burritos", "Steak"]
+
+# Puts my favorite food
+puts foods[2]
+
+# Construct an array with the colors of the rainbow (ROYGBIV)
+colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+
+# Slice the colors Red, Orange, and Yellow out of the array
+
+colors.slice!(0, 3)
